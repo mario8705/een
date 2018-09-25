355caf37f71a7296bcc9b7ed4b5854f13583aa5a
Alexis Lavaud <alexislavaud191@gmail.com>
2018-09-25 09:27:08 +0200

    Shadows

--- src/Game.js
+++ src/Game.js
@@ -1,24 +1,16 @@
 import * as THREE from 'three';
-import GLTFLoader from 'three-gltf-loader';
-import Sky from 'three-sky';
-const OrbitControls = require('three-orbit-controls')(THREE);
+import TestScene from './TestScene';
 
 class Game {
     constructor(width = 1280, height = 720) {
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(width, height);
-
         this.renderer.setPixelRatio(window.devicePixelRatio);
 
+        this.width = width;
+        this.height = height;
+
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(60, width / height, 0.3, 100.0);
-        this.scene.add(this.camera);
-
-        this.camera.position.set(0, 0, 30);
-
-        const sky = new Sky();
-        sky.scale.setScalar(80);
-        this.scene.add(sky);
 
         // DEBUG
         this.renderer.setClearColor(0x336699, 1.0);
@@ -28,34 +20,33 @@ class Game {
         this.init();
     }
 
+    loadScene(currentScene) {
+        this.currentScene = currentScene;
+        this.camera = currentScene.createCamera(this.width / this.height);
+        this.scene = new THREE.Scene();
+        this.scene.add(this.camera);
+
+        currentScene.createScene(this.scene, this.renderer);
+    }
+
     init() {
-        const loader = new GLTFLoader();
-        loader.load('assets/ship_light.gltf', object => this.scene.add(object.scene));
-
-        var lumiere = new THREE.DirectionalLight( 0xffffff, 1.0 );
-        lumiere.position.set( 50, 0, 100 );
-        this.scene.add( lumiere );
-
-        this.scene.add(new THREE.AmbientLight(0x606060));
-
-        this.camera.rotateY(90);
-        this.camera.position.set(30, 0, 0);
-
-        const controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.loadScene(new TestScene());
     }
 
     run() {
+        let lastFrameTime = 0;
+
         const callRender = timestamp => {
+            const tpf = (timestamp - lastFrameTime) / 1000.0;
+            lastFrameTime = timestamp;
+
+            this.currentScene.update(tpf);
             this.renderer.render(this.scene, this.camera);
 
             window.requestAnimationFrame(callRender);
         }
 
         callRender(0);
-    }
-
-    render() {
-
     }
 }
 
---
+++ src/TestScene.js
@@ -0,0 +1,51 @@
+import * as THREE from 'three';
+import GLTFLoader from 'three-gltf-loader';
+import Sky from 'three-sky';
+const OrbitControls = require('three-orbit-controls')(THREE);
+
+class TestScene {
+    createCamera(aspect) {
+        return this.camera = new THREE.PerspectiveCamera(60, aspect, 0.3, 1000.0);
+    }
+
+    createScene(scene, renderer) {
+        const loader = new GLTFLoader();
+        loader.load('assets/ship_light.gltf', gltf => {
+            gltf.scene.traverse(node => {
+                if (node instanceof THREE.Mesh) {
+                    node.castShadow = true;
+                    node.receiveShadow = true;
+                }
+            });
+
+            scene.add(gltf.scene);
+        });
+
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+        var sunLight = new THREE.DirectionalLight(0xffffff, 1.0);
+        sunLight.position.set(50, 50, 50);
+        scene.add(sunLight);
+
+        sunLight.shadow.mapSize.width = 2048;
+        sunLight.shadow.mapSize.height = 2048;
+        sunLight.shadow.camera.near = 3;
+        sunLight.shadow.camera.far = this.camera.far;   
+       // sunLight.shadow.bias = 0.00039;
+        sunLight.castShadow = true;
+
+        scene.add(new THREE.AmbientLight(0x606060));
+
+        this.camera.rotateY(90);
+        this.camera.position.set(30, 0, 0);
+
+        const controls = new OrbitControls(this.camera, renderer.domElement);
+    }
+
+    update(tpf) {
+
+    }
+}
+
+export default TestScene;
