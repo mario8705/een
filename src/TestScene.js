102af5ed2163cd83799f70b14bce96fec6a70646
Alexis Lavaud <alexislavaud191@gmail.com>
2018-09-25 10:08:57 +0200

    Added sky

--- src/TestScene.js
+++ src/TestScene.js
@@ -24,16 +24,16 @@ class TestScene {
         renderer.shadowMap.enabled = true;
         renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-        var sunLight = new THREE.DirectionalLight(0xffffff, 1.0);
-        sunLight.position.set(50, 50, 50);
-        scene.add(sunLight);
+        this.sunLight = new THREE.DirectionalLight(0xffffff, 1.0);
+        this.sunLight.position.set(50, 50, 50);
+        scene.add(this.sunLight);
 
-        sunLight.shadow.mapSize.width = 2048;
-        sunLight.shadow.mapSize.height = 2048;
-        sunLight.shadow.camera.near = 3;
-        sunLight.shadow.camera.far = this.camera.far;
+        this.sunLight.shadow.mapSize.width = 2048;
+        this.sunLight.shadow.mapSize.height = 2048;
+        this.sunLight.shadow.camera.near = 3;
+        this.sunLight.shadow.camera.far = this.camera.far;
        // sunLight.shadow.bias = 0.00039;
-        sunLight.castShadow = true;
+        this.sunLight.castShadow = true;
 
         scene.add(new THREE.AmbientLight(0x606060));
 
@@ -42,10 +42,43 @@ class TestScene {
 
         const controls = new OrbitControls(this.camera, renderer.domElement);
         controls.enablePan = false;
+
+        this.sky = new Sky();
+        scene.add(this.sky);
+        this.sky.scale.addScalar(90);
+
+        this.sunSphere = new THREE.Mesh(
+            new THREE.SphereBufferGeometry( 20000, 16, 8 ),
+            new THREE.MeshBasicMaterial( { color: 0xffffff } )
+        );
+        this.sunSphere.position.y = - 700000;
+        this.sunSphere.visible = false;
+        scene.add( this.sunSphere );
     }
 
-    update(tpf) {
+    time = 0;
 
+    update(tpf) {
+        const uniforms = this.sky.material.uniforms;
+        uniforms.turbidity.value = 10;
+        uniforms.rayleigh.value = 2;
+        uniforms.luminance.value = 1;
+        uniforms.mieCoefficient.value = 0.005;
+        uniforms.mieDirectionalG.value = 0.8;
+
+        let inclination = 0.49;
+        let azimuth = 0.25;
+
+        const distance = 50;
+
+        const theta = Math.PI * ( inclination - 0.5 );
+        const phi = 2 * Math.PI * ( azimuth - 0.5 );
+        this.sunLight.position.x = distance * Math.cos( phi );
+        this.sunLight.position.y = distance * Math.sin( phi ) * Math.sin( theta );
+        this.sunLight.position.z = distance * Math.sin( phi ) * Math.cos( theta );
+        uniforms.sunPosition.value.copy( this.sunLight.position );
+
+        this.time += tpf;
     }
 }
 
